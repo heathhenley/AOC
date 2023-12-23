3abb503bcaa5d645b721f1e00eae4c1f97c316e0
Heath Henley <heath.j.henley@gmail.com>
2023-12-23 15:28:22 -0500

    Part 2!

--- day23/23.py
+++ day23/23.py
@@ -1,3 +1,4 @@
+import collections
 import sys
 import time
 
@@ -64,6 +65,78 @@ def get_end(grid: list) -> tuple[int, int]:
       if grid[-1][j] == '.':
         return (len(grid) - 1, j)
 
+def get_adjacencies(grid: list) -> dict[tuple[int, int], list[tuple[int, int]]]:
+  adjacencies = collections.defaultdict(list)
+  for i in range(len(grid)):
+    for j in range(len(grid[0])):
+      if grid[i][j] == '#':
+        continue
+      for dx, dy in get_dirs(grid[i][j]):
+        tx, ty = i + dx, j + dy
+        if tx < 0 or ty < 0 or tx >= len(grid) or ty >= len(grid[0]):
+          continue
+        if grid[tx][ty] == '#':
+          continue
+        adjacencies[(i, j)].append((tx, ty))
+  return adjacencies
+
+def make_graph(grid: list) -> dict[tuple[int, int], list[tuple[int, int]]]:
+  # make an adjacency list for the graph, where the key is the node and the
+  # value is a list of all the nodes it can go to
+  adjacencies = get_adjacencies(grid)
+  # find intersections (nodes with 3 or more neighbors)
+  intersections = []
+  for node, neighbors in adjacencies.items():
+    if len(neighbors) >= 3:
+      intersections.append(node)
+  # find the start / end
+  start, end = get_start(grid), get_end(grid)
+
+  # key points we don't want to squish together
+  key_points = [start, *intersections, end]
+
+  # new graph with the intersections and start / end and all
+  # other's squished together
+  graph = collections.defaultdict(dict)
+  for node in key_points:
+    queue = [(node, 1)]
+    visited = set()
+    while queue:
+      n, dist = queue.pop(0)
+      if n in visited:
+        continue
+      visited.add(n)
+      for neighbor in adjacencies[n]:
+        if neighbor == node:
+          continue
+        if neighbor not in key_points:
+          queue.append((neighbor, dist + 1))
+          continue
+        graph[node][neighbor] = dist
+        graph[neighbor][node] = dist
+  return graph
+
+
+def walk_graph_dfs(
+    graph: dict[tuple[int, int], list[tuple[int, int]]], start, end) -> int:
+  """ This works in that it finds the correct, max length path, but it
+  there is a loop somewhere that, I haven't figured out the stop condition."""
+  stack = [(start, set(), 0)]
+  longest = 0
+  while stack:
+    node, path, dist = stack.pop()
+    if node == end:
+      longest = max(dist, longest)
+      #print(f"Found: {dist}, longest: {longest}")
+      continue
+    if node in path:
+      continue
+    path.add(node)
+    for neighbor, weight in graph[node].items():
+      if neighbor in path:
+        continue
+      stack.append((neighbor, path.copy(), dist + weight))
+  return longest
 
 def part1(filename: str) -> int:
   lines = read_input(filename)
@@ -74,8 +147,9 @@ def part1(filename: str) -> int:
 def part2(filename: str) -> int:
   lines = read_input(filename)
   grid = [[x for x in line] for line in lines]
-  start, end = get_start(grid), get_end(grid)
-  return walk_grid_dfs(grid, start, end, slopes=False)
+  start, end = get_start(grid), get_end(grid) 
+  graph = make_graph(grid)
+  return walk_graph_dfs(graph, start, end)
 
 
 def main():
