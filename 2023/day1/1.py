fac5e4f4c7786cbe9689e259f5707898103d2e4e
Heath Henley <heath.j.henley@gmail.com>
2024-10-26 18:47:16 -0400

    Fix 2023 day 1

--- 2023/day1/1.py
+++ 2023/day1/1.py
@@ -1,31 +1,13 @@
+# Advent of Code 2023: Day 1 - Calibration
+# https://adventofcode.com/2023/day/1
+# I reworked this to clean it up, and also solve in a slightly different way
+# than I did the first time around (which you can see in the git history).
+
 import re
+from common.utils import problem_harness, timeit, read_input
 
-# This was before I had a nice little template and committed to making things
-# a little more organized. I just slapped out this code to get the answer
 
-x = []
-with open("input.txt", "r") as f:
-    x = [i.strip() for i in f.readlines()]
-
-# Part 1
-ans = 0
-for s in x:
-  d = []
-  for i in range(len(s)):
-    if s[i] in "0123456789":
-      d.append(s[i])
-      break
-  s = s[::-1]
-  for i in range(len(s)):
-    if s[i] in "0123456789":
-      d.append(s[i])
-      break
-  print(s, d)
-  ans += int("".join(d))
-print("part1:", ans)
-
-# Part 2
-m = {
+words_to_numbers = {
   "one": 1,
   "two": 2,
   "three": 3,
@@ -36,30 +18,62 @@ m = {
   "eight": 8,
   "nine": 9,
 }
+digits = "0123456789"
 
-ans = 0
-for s in x:
-  d_idx = []
-  for k, v in m.items():
-    if k in s:
-      d_idx.extend([[a.start(), k] for a in list(re.finditer(k, s))])
-  s1 = s[::] 
-  s2 = s[::]
-  d_idx.sort()
-  if d_idx:
-    start_idx, n = d_idx[0][0], len(d_idx[0][1])
-    s1 = s[:start_idx] + str(m[d_idx[0][1]]) + s[start_idx+n:]
-    start_idx, n = d_idx[-1][0], len(d_idx[-1][1])
-    s2 = s[:start_idx] + str(m[d_idx[-1][1]]) + s[start_idx+n:]
-  d = []
-  for i in range(len(s1)):
-    if s1[i] in "123456789":
-      d.append(s1[i])
-      break
-  s2 = s2[::-1]
-  for i in range(len(s2)):
-    if s2[i] in "123456789":
-      d.append(s2[i])
-      break
-  ans += int("".join(d)) 
-print("part2:", ans)
+
+def get_number(match: str) -> int:
+  try:
+    return int(match)
+  except ValueError:
+    return words_to_numbers[match]
+
+
+def get_all_match_idx(
+    line: str, targets: list, idx_accum: list = [], curr: int = 0) -> list:
+  if not line or not targets or curr == len(line):
+    return idx_accum
+  for t in targets:
+    if t in line[curr:]:
+      idx = line[curr:].index(t) + curr
+      idx_accum.append({"index": idx + curr, "matched": get_number(t)})
+  return get_all_match_idx(line, targets, idx_accum, curr + 1)
+
+
+def get_calibration_value(line: str) -> int:
+  try:
+    left = int(re.search(r'\d{1}', line).group(0))
+    right = int(re.search(r'\d{1}', line[::-1]).group(0))
+  except AttributeError:
+    print("no digits found in line:", line)
+    return 0
+  return left * 10 + right
+
+
+def get_calibration_value_with_words(line: str) -> int:
+  targets = list(words_to_numbers.keys()) + list(digits)
+  matches = []
+  # find the indices of all the matches (words or digits)
+  if not(matches := get_all_match_idx(line, targets, matches)):
+    raise ValueError("This should not happen if the input is correct")
+  # then grab the first and last match idx and use them
+  matches.sort(key=lambda x: x["index"])
+  return matches[0]["matched"] * 10 + matches[-1]["matched"]
+  
+
+@timeit
+def part1(filename: str) -> int:
+  print("Using input file:", filename)
+  return sum(map(get_calibration_value, read_input(filename)))
+
+
+@timeit
+def part2(filename: str) -> int:
+  return sum(map(get_calibration_value_with_words, read_input(filename)))
+
+
+def main():
+  problem_harness(part1, part2)
+
+
+if __name__ == '__main__':
+  main()
