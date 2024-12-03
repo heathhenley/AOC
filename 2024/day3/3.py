77e724c27638e77d92ef0f8198601daaef775e40
Heath Henley <heath.j.henley@gmail.com>
2024-12-03 08:53:41 -0500

    minor tweaks

--- 2024/day3/3.py
+++ 2024/day3/3.py
@@ -16,41 +16,27 @@ def part1(filename: str) -> int:
 @timeit
 def part2(filename: str) -> int:
   mult_ptn = re.compile(r"mul\((\d+),(\d+)\)")
+  do_ptn = r"(do\(\))|(don't\(\))"
 
-  # this was the source of my hour of suffering - put the whole file on a line
-  # so the indexes are good to go and we don't restart the switches every line (
-  # eg it's only 'enabled' on the very start of the 'program', NOT the start of 
-  # every line)
+  # process as a single string (easier for idx's)
   line = "".join(read_input(filename)) 
 
   mults = []
-  controls = [(0, True)] # start with do()
-  s = 0
   for m in mult_ptn.finditer(line):
-    idx = m.start(0)
     x, y = m.groups()
-    x, y = int(x), int(y)
-    mults.append((x, y, idx))
+    mults.append((int(x), int(y), m.start(0)))
 
-  do = r"(do\(\))"
-  dont = r"(don't\(\))"
-  for m in re.finditer(do, line):
-    controls.append((m.start(0), True))
-  for m in re.finditer(dont, line):
-    controls.append((m.start(0), False))
-
-  mults.sort(key=lambda x: x[2])
-  controls.sort(key=lambda x: x[0])
-  controls.append((len(line), True))
+  controls = [(0, True)] # on by default
+  for m in re.finditer(do_ptn, line):
+    controls.append((m.start(0), m.group(0) == "do()"))
+  controls.append((len(line), True)) # so we go to the end
   
-  # we want to take the first control off, and apply
-  # it to all the mults that come after it, but before the next control
+  s = 0
   for i in range(len(controls) - 1):
     start, on = controls[i]
     end = controls[i+1][0]
-    for x, y, idx in mults:
-      if start <= idx < end and on:
-        s += x * y
+    if on:
+      s += sum(x * y for x, y, idx in mults if start <= idx < end)
   return s
 
 
