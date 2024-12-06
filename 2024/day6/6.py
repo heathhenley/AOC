2bc2404064d69bee2f52c9105e570dfdfcfde187
Heath Henley <heath.j.henley@gmail.com>
2024-12-06 01:43:51 -0500

    clean up the python version a bit

--- 2024/day6/6.py
+++ 2024/day6/6.py
@@ -1,5 +1,5 @@
 from common.utils import problem_harness, timeit, read_input
-from functools import cache
+
 
 # if you are this arrow when you hit a corner, you will turn to this direction
 arrow_turns = { '^': '>', 'v': '<', '<': '^', '>': 'v' }
@@ -12,6 +12,7 @@ arrow_dir = {
   '<': (0, -1),
   '>': (0, 1)
 }
+
 
 def valid(i, j, rows, cols):
   return i >= 0 and i < rows and j >= 0 and j < cols
@@ -28,7 +29,7 @@ def get_visited(grid, g_pos):
     if (i, j, curr_dir) in visited:
       return visited, True
     visited.add((i, j, curr_dir))
-    # try move
+    # try move if we can
     dr, dc = arrow_dir[curr_dir]
     tmp_i, tmp_j = i + dr, j + dc
     if tmp_i < 0 or tmp_i >= rows or tmp_j < 0 or tmp_j >= cols:
@@ -41,58 +42,37 @@ def get_visited(grid, g_pos):
     curr_dir = arrow_turns[curr_dir]
   return visited, False
 
-@timeit
-def part1(filename: str) -> int:
-  grid = [list(line.strip()) for line in read_input(filename)]
-  rows = len(grid)
-  cols = len(grid[0])
-  for i in range(rows):
-    for j in range(cols):
-      if grid[i][j] in ['^', 'v', '<', '>']:
-        g_pos = (i, j)
-        break 
-  return len(set([(x, y) for x, y, _ in list(get_visited(grid, g_pos)[0])]))
-
-
-@timeit
-def part2(filename: str) -> int:
-  grid = []
-  for line in read_input(filename):
-    grid.append(list(line.strip()))
-
+def find_start(grid):
   rows = len(grid)
   cols = len(grid[0])
   for i in range(rows):
     for j in range(cols):
       if grid[i][j]  in ['^', 'v', '<', '>']:
-        g_pos = (i, j)
-        break
-    
-  # - if we get to the same position twice heading the same direction we found a
-  #   cycle
-  # - we know that the new obstacle will have to be on the original path
-  # - make a function that returns: the visited set for a grid, and whether
-  #   it terminated in a cycle
-  # - try adding an obstacle to any of the positions in the original path?
-  # - once we've visited a node going in a direction it's a cycle
-  # - answer is too low (1893 is too low on real input)
-  #   - tried too add to any position in the entire grid to check the assumption
-  #   - that it needs to be in the original path (we get the same answer) - so
-  #   - that suggests to me that there's something wrong how I'm counting the
-  #   - cycles or the visited positions, maybe existing too early in a rare case
-  original_visited = list(get_visited(grid, g_pos)[0])
-  # try to put an obstacle in each of the originally visited positions
-  # test for a cycle, count them
-  start_i, start_j = g_pos
+        return (i, j)
+  return None 
+
+@timeit
+def part1(filename: str) -> int:
+  grid = [list(line.strip()) for line in read_input(filename)]
+  start = find_start(grid) 
+  return len(set([(x, y) for x, y, _ in list(get_visited(grid, start)[0])]))
+
+
+@timeit
+def part2(filename: str) -> int:
+  grid = [list(line.strip()) for line in read_input(filename)]
+  start = find_start(grid)
+   
+  original_visited = list(get_visited(grid, start)[0])
   obstacle_positions = set()
+  # we only need to check the places we visited on the original path
   for (i, j, _) in original_visited:
-      if (i, j) == (start_i, start_j):
+      if (i, j) == start:
         continue
       new_grid = [row[:] for row in grid]
       new_grid[i][j] = "#"
       # test if there is a cycle
-      _, cycle = get_visited(new_grid, (start_i, start_j))
-      if cycle:
+      if get_visited(new_grid, start)[1]:
         obstacle_positions.add((i, j))
   return len(obstacle_positions)
 
