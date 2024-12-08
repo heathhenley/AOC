d498ec8a504bed686da8526c4a0db09b4a6df53c
Heath Henley <heath.j.henley@gmail.com>
2024-12-08 08:58:10 -0500

    use one function for both

--- 2024/day8/8.py
+++ 2024/day8/8.py
@@ -6,6 +6,7 @@ from common.utils import problem_harness, timeit, read_input
 def valid(grid, x, y):
   return 0 <= x < len(grid) and 0 <= y < len(grid[0])
 
+
 def find_antennas(grid):
   antennas = defaultdict(list)
   for r in range(len(grid)):
@@ -14,61 +15,36 @@ def find_antennas(grid):
         antennas[grid[r][c]].append((r, c))
   return antennas
 
-def find_antinodes(grid, antennas):
-  anti_nodes = defaultdict(set)
-  for freq, ants in antennas.items():
-    if len(ants) <= 1:
-      continue
-    for i in range(len(ants)):
-      for j in range(i + 1, len(ants)):
-        dr = ants[j][0] - ants[i][0]
-        dc = ants[j][1] - ants[i][1]
-        n1r = ants[i][0] + dr * 2
-        n1c = ants[i][1] + dc * 2
-        n2r = ants[j][0] - dr * 2
-        n2c = ants[j][1] - dc * 2
-        if valid(grid, n1r, n1c):
-          anti_nodes[freq].add((n1r, n1c))
-        if valid(grid, n2r, n2c):
-          anti_nodes[freq].add((n2r, n2c))
-  return anti_nodes
 
-def find_antinodes_p2(grid, antennas):
+def find_antinodes(grid, antennas, all_mults=False):
   anti_nodes = defaultdict(set)
   for freq, ants in antennas.items():
     if len(ants) <= 1:
       continue
     for i in range(len(ants)):
       for j in range(i + 1, len(ants)):
-        # find the slope between them
-        dr = ants[j][0] - ants[i][0]
-        dc = ants[j][1] - ants[i][1]
-        # need to reduce the slope to simplest integer form
-        # find the gcd of the two numbers
+        dr, dc = ants[j][0] - ants[i][0], ants[j][1] - ants[i][1]
+
+        if not all_mults:
+          n1r, n1c = ants[i][0] + 2 * dr, ants[i][1] + 2 * dc
+          n2r, n2c = ants[j][0] - 2 * dr, ants[j][1] - 2 * dc
+          if valid(grid, n1r, n1c):
+            anti_nodes[freq].add((n1r, n1c))
+          if valid(grid, n2r, n2c):
+            anti_nodes[freq].add((n2r, n2c))
+          continue
+
         gcd = math.gcd(dr, dc)
         dr //= gcd
         dc //= gcd
-        # generate the 'anti-nodes' stepping by dr, dc from an antenna in
-        # both direction until we hit the edge of the grid
-        # dir 1
-        k = 0
-        while True:
-          n1r = ants[i][0] + dr * k
-          n1c = ants[i][1] + dc * k
-          if not valid(grid, n1r, n1c):
-            break
-          anti_nodes[freq].add((n1r, n1c))
-          k += 1
-        
-        # dir 2
-        k = 0
-        while True:
-          n1r = ants[i][0] - dr * k
-          n1c = ants[i][1] - dc * k
-          if not valid(grid, n1r, n1c):
-            break
-          anti_nodes[freq].add((n1r, n1c))
-          k += 1
+        for d in [1, -1]:
+          k = 0
+          while True:
+            n1r, n1c = ants[i][0] + dr * k * d, ants[i][1] + dc * k * d
+            if not valid(grid, n1r, n1c):
+              break
+            anti_nodes[freq].add((n1r, n1c))
+            k += 1
   return anti_nodes
 
 
@@ -76,26 +52,25 @@ def find_antinodes_p2(grid, antennas):
 def part1(filename: str) -> int:
   grid = [list(line) for line in read_input(filename)]
   antennas = find_antennas(grid)
-  anti_nodes = find_antinodes(grid, antennas)
-  all = set()
-  for v in anti_nodes.values():
-    all.update(v)
-  return len(all)
+  all_nodes = set()
+  for v in find_antinodes(grid, antennas).values():
+    all_nodes.update(v)
+  return len(all_nodes)
 
 
 @timeit
 def part2(filename: str) -> int:
   grid = [list(line) for line in read_input(filename)]
   antennas = find_antennas(grid)
-  anti_nodes = find_antinodes_p2(grid, antennas)
-  all = set()
-  for v in anti_nodes.values():
-    all.update(v)
-  return len(all)
+  all_nodes = set()
+  for v in find_antinodes(grid, antennas, all_mults=True).values():
+    all_nodes.update(v)
+  return len(all_nodes)
 
 
 def main():
   problem_harness(part1, part2)
+
 
 if __name__ == '__main__':
   main()
