0aaad360326c56f3fb7f022f58a9033585dc20bb
Heath Henley <heath.j.henley@gmail.com>
2024-12-09 03:31:53 -0500

    well that was kind of rough

--- 2024/day9/9.py
+++ 2024/day9/9.py
@@ -1,68 +1,164 @@
+from dataclasses import dataclass
 from common.utils import problem_harness, timeit, read_input
 
+
+@dataclass
+class MemoryBlock:
+  fileidx: int = None
+
+@dataclass
+class MemoryBlockWithSize:
+  fileidx: int = None
+  size: int = 0
+
+
+def print_blocks(blocks):
+  s = ''
+  for block in blocks:
+    s += str(block.fileidx) if block.fileidx is not None else '.'
+  print(s)
+
+def print_blocks_with_size(blocks):
+  s = ''
+  for block in blocks:
+    c = str(block.fileidx) if block.fileidx is not None else '.'
+    s += c * block.size
+  print(s)
 
 @timeit
 def part1(filename: str) -> int:
   disk_map = [int(x) for x in list(read_input(filename)[0])]
-  print(disk_map)
-  # even indices are the files - their file id is their pos in the list
-  files = {i // 2: disk_map[i] for i in range(0, len(disk_map), 2)}
-  print(files)
-  # odd indices are blocks of free space
-  free = {i // 2 + 1: disk_map[i] for i in range(1, len(disk_map), 2)}
-  print(free)
-  print(sum(free.values()))
-  # start with the left most free block
-  # start with the right most file
-  # take a block from the right most file idx, put it in the current free idx
-  # move to the next free idx were' at 0 fro this block
-  new_files = {}
-  curr_free_idx = 1
-  stack = [(idx, files[idx]) for idx in files]
-  while stack:
-    # fill the current free idx with the right most fild idx
-    file_idx, blocks = stack.pop()
-    
-    # find next free idx
-    while free[curr_free_idx] == 0:
-      curr_free_idx += 2
-      if curr_free_idx >= len(free):
-        break
-
-    # fill the free idx with the file idx
-    free[curr_free_idx] -= 1
-    new_files[curr_free_idx] = new_files.get(curr_free_idx, []) + [file_idx]
-
-    # if the file idx has more blocks, put it back in the stack 
-    if blocks > 1:
-      stack.append((file_idx, blocks - 1))
-
-    # how do we know when we're done?
-  
-  # merge the original files with the new files
-  print(new_files)
-  print(files)
-
-  n = max(max(files.keys()), max(new_files.keys()))
-  s = ""
-  for i in range(0, n):
-    print(s)
-    if i % 2 == 0:
-      s += str(files[i // 2])
+  memory = []
+  for idx, d in enumerate(disk_map):
+    if idx % 2 == 0:
+      for _ in range(d):
+        memory.append(MemoryBlock(
+          fileidx=idx // 2
+        ))
     else:
-      print(new_files[i])
-      s += "".join([str(x) for x in new_files[i]])
-  print(s)
+      for _ in range(d):
+        memory.append(MemoryBlock(fileidx=None))
 
+  left, right = 0, len(memory) - 1
+  while left < right:
+    if memory[left].fileidx is not None:
+      left += 1
+    elif memory[right].fileidx is None:
+      right -= 1
+    else:
+      memory[left] = memory[right]
+      memory[right] = MemoryBlock(fileidx=None)
+      left += 1
+      right -= 1
 
-
-
-  return 0
+  checksum = 0
+  for idx, block in enumerate(memory):
+    if block.fileidx is not None:
+      checksum += idx * block.fileidx
+  return checksum
 
 
 @timeit
 def part2(filename: str) -> int:
-  return 0
+  disk_map = [int(x) for x in list(read_input(filename)[0])]
+  memory = []
+  for idx, d in enumerate(disk_map):
+    #print(idx, d)
+    if idx % 2 == 0:
+      memory.append(MemoryBlockWithSize(
+        fileidx=idx // 2,
+        size=int(d)
+      ))
+    else:
+      memory.append(MemoryBlockWithSize(fileidx=None, size=int(d)))
+
+  #print_blocks_with_size(memory)
+
+#  right = len(memory) - 1
+#  while right > 0:
+#    if memory[right].fileidx is None:
+#      right -= 1
+#      continue
+#    block_size = 0
+#    k = right
+#    fid = memory[right].fileidx
+#    while memory[k].fileidx == fid:
+#      block_size += 1
+#      k -= 1
+#    # find the first free block big enough to fit the current block
+#    for start, length in map.items():
+#      if block_size <= length:
+#        break
+#    else:
+#      # need to move along to the nex file - we can't fit the current one
+#      right -= block_size
+#      continue
+#
+#    
+#    # find the left most block that can fit the current file
+#    for idx, block in enumerate(memory):
+#      if block.fileidx is None:
+#        start = idx
+#        while memory[idx].fileidx is None:
+#          idx += 1
+#        if idx - start >= block_size:
+#          break
+#    else:
+#      # we can't fit the file anywhere, need to skip to the next file
+#      while fid == memory[right].fileidx:
+#        right -= 1
+#        continue
+#    # we can fit the file blocks, so move them
+#    for _ in range(block_size):
+#      print(start, right)
+#      memory[start] = memory[right]
+#      memory[right] = MemoryBlock(fileidx=None)
+#      start += 1
+#      right -= 1
+#
+#    if fid == 0:
+#      break
+#
+#  checksum = 0
+#  for idx, block in enumerate(memory):
+#    if block.fileidx is not None:
+#      checksum += idx * block.fileidx
+#  return checksum
+
+  # go backwards to try to move each one once?
+  left, right = 0, len(memory) - 1
+  while right > 0:
+    for left in range(right):
+      ml = memory[left]
+      mr = memory[right]
+      if mr.fileidx is not None and ml.fileidx is None:
+        # if there is space - take this one 
+        if ml.size >= mr.size:
+          ml.size -= mr.size
+          memory = (
+            memory[:left]
+            + [MemoryBlockWithSize(fileidx=mr.fileidx, size=mr.size)]
+            + memory[left:]
+          )
+          mr.fileidx = None
+    right -= 1
+
+  #print_blocks_with_size(memory)
+
+
+  # make it like part 1 just get it over with
+  m = []
+  for idx, block in enumerate(memory):
+    for _ in range(block.size):
+      m.append(MemoryBlock(fileidx=block.fileidx))
+
+  checksum = 0
+  for idx, block in enumerate(m):
+    if block.fileidx is not None:
+      checksum += idx * block.fileidx
+  return checksum
+
+
 
 
 def main():
