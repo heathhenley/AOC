83c086a850efa280e42402d9e2bdca1274e0aef7
Heath Henley <heath.j.henley@gmail.com>
2025-06-06 19:08:20 -0400

    Rename old version to clean up - dry out runner

--- 2016/ocaml_aoc/bin/main.ml
+++ 2016/ocaml_aoc/bin/main.ml
@@ -1,15 +1,19 @@
+let days : (string * (module Solution.Day)) list =
+  [ ("1", (module Day01))
+  ; ("6", (module Day06))
+  ; ("7", (module Day07))
+  ]
+
+let run_day day_module filename =
+  let module D = (val day_module : Solution.Day) in
+  Utils.Timing.time_function D.part1 filename;
+  Utils.Timing.time_function D.part2 filename
+
 let () =
   match Sys.argv with
-  | [|_; day; filename|] -> (
-      match day with
-      | "1" ->
-          Printf.printf "\nDay 1\n";
-          Utils.Timing.time_function Day01.part1 filename;
-          Utils.Timing.time_function Day01.part2 filename
-      | "6" ->
-          Printf.printf "\nDay 6\n";
-          Utils.Timing.time_function Day06.part1 filename;
-          Utils.Timing.time_function Day06.part2 filename
-      | _ -> Printf.printf "Day %s not implemented\n" day
-    )
+  | [| _; day_str; filename |] -> (
+      Printf.printf "\nDay %s\n" day_str;
+      match List.assoc_opt day_str days with
+      | Some day_module -> run_day day_module filename
+      | None -> Printf.printf "Day %s not implemented yet\n" day_str)
   | _ -> Printf.printf "Usage: %s <day> <filename>\n" Sys.argv.(0)
---
+++ 2016/ocaml_aoc/lib/solution.ml
@@ -0,0 +1,4 @@
+module type Day = sig
+  val part1 : string -> unit
+  val part2 : string -> unit
+end 
