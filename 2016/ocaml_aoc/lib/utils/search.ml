5f665bd6c68254cc0879ecfe236d41cf2c8bad68
Heath Henley <heath.j.henley@gmail.com>
2025-11-15 00:43:04 -0500

    make key function customizable

--- 2016/ocaml_aoc/lib/utils/search.ml
+++ 2016/ocaml_aoc/lib/utils/search.ml
@@ -1,23 +1,22 @@
 type 'r visit_result = [`Stop of 'r | `Continue ]
 
-let bfs ?(size=10) start ~neighbors ~on_visit =
-  let q = Queue.create () in
-  let v = Hashtbl.create size in
-  Queue.add (start, 0) q;
-  Hashtbl.add v start ();
+let bfs ?(size=10) ?(key=fun x -> x) start ~neighbors ~on_visit =
+  let queue = Queue.create () in
+  let visited = Hashtbl.create size in
+  Queue.add (start, 0) queue;
+  Hashtbl.add visited (key start) ();
   let rec bfs' () =
-    if Queue.is_empty q then None
+    if Queue.is_empty queue then None
     else 
-      let node, dist = Queue.pop q in
+      let node, dist = Queue.pop queue in
       match on_visit node dist with
       | `Stop r -> Some r
       | `Continue ->
         neighbors node
-        |> List.iter (fun n ->
-              if not (Hashtbl.mem v n) then (
-                Queue.add (n, dist + 1) q;
-                Hashtbl.add v n ()));
+        |> List.iter (fun neighbor ->
+              if not (Hashtbl.mem visited (key neighbor)) then (
+                Queue.add (neighbor, dist + 1) queue;
+                Hashtbl.add visited (key neighbor) ()));
         bfs' ()
   in
   bfs' ()
-
