4ccf045a1c5839f19c159c0b2a88235f60ecbb3b
Heath <heath.j.henley@gmail.com>
2025-12-02 18:28:57 -0500

    Playing around with day 2 soln
    I wanted to try it with math without using the regex - but at least the
    way I set it up, that was only better for part 1. Part 2 is still faster
    with the regex.

--- 2025/lib/day02.ml
+++ 2025/lib/day02.ml
@@ -1,41 +1,47 @@
-module Day02_impl : Solution.Day = struct
-  let is_valid regex id = not (Str.string_match regex id 0)
+module Day02_impl = struct
+  let is_valid_re regex id = not (Str.string_match regex (string_of_int id) 0)
 
-  let check_range start stop f =
+  let check_range start stop valid =
     let rec aux i acc =
       if i > stop then acc
-      else if f (string_of_int i) then aux (i + 1) acc
+      else if valid i then aux (i + 1) acc
       else aux (i + 1) (i :: acc)
     in
     aux start []
 
   let parse s = String.split_on_char '-' s |> List.map int_of_string
 
-  let solve filename checker =
+  let solve filename valid =
     filename
     |> Utils.Input.read_file_to_string
     |> String.split_on_char ','
     |> List.map parse
     |> List.fold_left
          (fun acc x ->
-           let invalid = check_range (List.hd x) (List.nth x 1) checker in
+           let invalid = check_range (List.hd x) (List.nth x 1) valid in
            acc + List.fold_left (fun acc2 y -> acc2 + y) 0 invalid)
          0
 
+  let halves n =
+    let nd = Utils.Math.digit_count n in
+    let half = nd / 2 in
+    (Utils.Math.get_subnumber n 0 half, Utils.Math.get_subnumber n half half)
+
   let part1 filename =
-    let checker = is_valid (Str.regexp {|^\(.+\)\1$|}) in
-    solve filename checker
-    |> Printf.printf "Part 1: %d\n"
+    let valid n =
+      if Utils.Math.digit_count n mod 2 <> 0 then true
+      else
+        let top, bottom = halves n in
+        top <> bottom
+    in
+    solve filename valid |> Printf.printf "Part 1: %d\n"
 
   let part2 filename =
-    let checker = is_valid (Str.regexp {|^\(.+\)\1+$|}) in
-    solve filename checker
-    |> Printf.printf "Part 2: %d\n"
-
+    let valid = is_valid_re (Str.regexp {|^\(.+\)\1+$|}) in
+    solve filename valid |> Printf.printf "Part 2: %d\n"
 end
 
 module Day02 : Solution.Day = Day02_impl
-
 include Day02_impl
 
 let () = Days.register "2" (module Day02)
---
+++ 2025/lib/utils/math.ml
@@ -0,0 +1,25 @@
+(* How many base 10 digits are in n? *)
+let digit_count n = log10 (float_of_int n) +. 1.0 |> int_of_float
+
+(* Return a number of len digits from n start at start - eg
+  get_subnumber 1234 0 2 = 12
+  get_subnumber 1234 2 2 = 34
+*)
+let get_subnumber n start len =
+  let num_digits = digit_count n in
+  if start + len > num_digits then
+    failwith "start + len is greater than the number of digits"
+  else
+    (* remove digits on the right by dividing by 10**(?) *)
+    (* we end at start + len, there are num_digits - (start + len) digits to remove - need to divide by 10**(num_digits - (start + len)) *)
+    let right_mask =
+      int_of_float @@ (10. ** float_of_int (num_digits - (start + len)))
+    in
+    (* note: can avoid the divide if we compute the mask accounting for it, need
+      tto think about the right offset - I think we substract however many we
+      trimmed on the right
+    *)
+    let left_mask =
+      int_of_float @@ (10. ** float_of_int (num_digits - start))
+    in
+    n / right_mask mod (left_mask / right_mask)
